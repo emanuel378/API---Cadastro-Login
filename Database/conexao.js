6ffe3c720ea3ac0806b7ff1da1b7c226dd0881a4
Emanuel Messias Vieira De Araújo <emanuelmvieira10@gmail.com>
2026-04-14 01:08:30 -0300

    1.0.0

--- Database/conexao.js
+++ Database/conexao.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2'
 
 const conexao = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORTA,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
